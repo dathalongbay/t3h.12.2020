8583113472c558782020093e393805a25041c98b
dat <dathalongbay@gmail.com>
2021-01-15 17:48:42 +0700

    hh

---
+++ es6/module1/square.js
@@ -0,0 +1,146 @@
+/*export const name = 'square';
+
+export function draw(ctx, length, x, y, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, length, length);
+
+    return {
+        length: length,
+        x: x,
+        y: y,
+        color: color
+    };
+}*/
+// xuat bien
+export const name = 'square';
+// xuat ham draw() export function and variable
+export function draw(ctx, length, x , y , color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, length, length);
+
+    return {
+        length: length,
+        x: x,
+        y: y,
+        color: color
+    };
+}
+
+export const name1 = 'square';
+
+export function drew() {
+
+}
+
+export function () {
+
+};
+// With call(), an object can use a method belonging to another object.
+// với call() 1 object có thể sử dụng phương thức thuộc về 1 object khác
+// với call() 1 object có thể sử dụng phương thức thuộc về 1 object khác
+// với call() 1 object có thể sử dụng phương thức thuộc về 1 object khác
+objectMaster.method.call(object2);
+
+objectMaster.method.call(object2);
+
+objectMaster.method.call(object2);
+
+objectMaster.method.call(object2);
+
+objectMaster.method.call(object2);
+
+
+objectMaster.method.call(object2);
+
+objectMaster.method.call(object2);
+
+
+objectMaster.method.call(object2);
+
+objectMaster.method.call(object2);
+
+objectMaster.method.call(object2);
+
+objectMaster.method.call(object2);
+
+objectMaster.method.call(object2);
+
+objectMaster.method.call(object2);
+
+objectMaster.method.call(object2);
+
+objectMaster.method.call(object2);
+
+objectMaster.method.call(object2);
+
+objectMaster.method.call(object2);
+
+ObjectMaster.method.call(object2);
+
+ObjectMaster.method.call(object2);
+
+
+objectMaster.method.apply(object2);
+
+objectMaster.method.apply(object2);
+
+
+objectMaster.method.apply(object2);
+
+ObjectMaster.method.call(object2);
+
+// call bind apply
+// call bind apply
+// call bind apply
+// call bind apply
+
+/*
+* Unlike the call() and apply() methods, the bind() method doesn’t immediately execute the function. It just returns the function.
+*
+* Không giống call() hay apply() phương thức bind() không chạy ngay lập tức
+* nó chỉ trả về 1 function
+*
+* Không giống call() hay apply() phương thức bind() không chạy ngay lập tức
+* nó chỉ trả về 1 function
+*
+*
+* es 6 ok => npm gulp webpack => nodejs expressjs =>
+* code lại javascript module tiêu chuẩn
+* vue js
+*
+*
+* */
+
+let run = runner.run.bind(flyer, 20);
+run();
+let run = runner.run.bind(flyer, 20);
+
+run();
+
+runner.run.bind(flyer, 20)();
+
+runner.run.bind(flyer, 20);
+
+runner.run.bind(flyer, 20)();
+
+// javascript callstack event loop
+// javascript callstack event loop
+// javascript callstack event loop run time
+runner.run.bind(fylder, 20)();
+
+runner.run.bind(flyer, 20)();
+
+runner.run.bind(flyer, 20);
+
+runner.run.bind(object, 20);
+
+fn.bind(thisArg, para1, para2)
+
+runner.ru.bind(object, 20)();
+
+
+
+
+
+
+
