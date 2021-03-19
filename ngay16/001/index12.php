5f6cea835acfa255876ccfbaf50e7b7d5806cee4
dat <dathalongbay@gmail.com>
2021-03-19 20:52:44 +0700

    11

---
+++ ngay16/001/index12.php
@@ -0,0 +1,20 @@
+<?php
+
+$domain = "vi";
+
+switch ($domain) {
+    case "en":
+        echo "nước anh";
+        break;
+    case "vi":
+        echo "việt nam";
+        break;
+    case "cn":
+        echo "trung quốc";
+        break;
+    case "jp":
+        echo "nhật bản";
+        break;
+    default:
+        echo "không xác định";
+}
