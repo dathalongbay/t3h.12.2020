15041a8c96657db6b03be5cb1f5fef1b9bba5a79
dat <dathalongbay@gmail.com>
2021-03-19 19:19:37 +0700

    111

--- ngay16/001/index1.php
+++ ngay16/001/index1.php
@@ -2,6 +2,18 @@
 // code php phải đặt trong script mở < ? php và script đóng là ? >
 // xuất ra màn hình trang web
 echo "Chương trình php đầu tiên";
+
+// http://localhost/t3h/001/index1.php
+
+
+// file code đặt trong C:\xampp\htdocs
+// vd1 : index1.php nằm trong đường dẫn C:\xampp\htdocs\index1.php
+// => http://localhost/index1.php
+// vd2: file C:\xampp\htdocs\abc\index1.php
+// url : http://localhost/abc/index1.php
+// vd3 : file C:\xampp\htdocs\abc\001\def.php
+// url : http://localhost/abc/001/def.php
+
 ?>
 
 
