f65ae27e3b87cdaeb9640b2113998aea8134b0b1
dat <dathalongbay@gmail.com>
2021-03-21 19:15:49 +0700

    111

---
+++ ngay17/001/index12.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+
+<strong>
+    Dùng vòng lặp foreach in ra các thông tin trong bảng html sau :
+</strong>
+<?php
+$cities = [
+  ["name" => "hà nội", "ds" => 5000000, "s" => 1000]  ,
+  ["name" => "hồ chí minh", "ds" => 8000000, "s" => 2000]  ,
+  ["name" => "thanh hóa", "ds" => 2000000, "s" => 100]  ,
+  ["name" => "nghệ an", "ds" => 1000000, "s" => 200]  ,
+  ["name" => "hà tĩnh", "ds" => 3000000, "s" => 800]  ,
+];
+
+echo "<pre>";
+print_r($cities);
+echo "</pre>";
+?>
+
+<table class="table">
+    <thead>
+    <tr>
+        <td>STT</td>
+        <td>tên TP</td>
+        <td>dân số</td>
+        <td>diện tích</td>
+        <td>mật độ người / km2</td>
+    </tr>
+    </thead>
+
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>hà nội</td>
+            <td>5000000</td>
+            <td>100</td>
+            <td>?</td>
+        </tr>
+
+
+
+
+
+    </tbody>
+</table>
+
+</body>
+</html>
