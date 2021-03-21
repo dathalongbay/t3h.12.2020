763a5175885856b0c675d1fce716f9f4e12bda8d
dat <dathalongbay@gmail.com>
2021-03-21 18:13:41 +0700

    111

---
+++ ngay17/001/index5.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <pre>
+        xuất ra chuỗi : chào 'hà nội' việt nam
+        giả sử chuỗi này nằm trong thẻ div có class .intro
+    </pre>
+
+    <div class='intro'>chào 'hà nội' việt nam</div>
+
+    <?php
+    echo "<div class='intro'>chào 'hà nội' việt nam</div>";
+
+    // cách 2
+    echo '<div class=\'intro\'>chào \'hà nội\' việt nam</div>';
+    ?>
+
+</body>
+</html>
---
+++ ngay17/001/index6.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="intro">chào 'hà nội' việt nam</div>
+
+    <?php
+    // áp dụng chuỗi bên trong vừa có nháy đơn vừa có nháy kép
+    echo "<div class=\"intro\">chào 'hà nội' việt nam</div>";
+    ?>
+
+</body>
+</html>
