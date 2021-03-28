8ab4e14e47a166c8c0a8f241ce73cd58fa412fa5
datdofpt <dat.doxuan@ggg.com.vn>
2021-03-28 20:06:05 +0700

    222

--- ngay19/002/index4.php
+++ ngay19/002/index4.php
@@ -2,3 +2,7 @@
 
 // tính xem còn bao nhiêu ngày, giờ nữa là tết dương lịch 2022
 
+// in ra 78 ngày 12 giờ
+// gợi ý dùng hàm time() lấy ra timestamp hiện tại
+// chú ý reset về múi giờ việt nam
+echo time();
