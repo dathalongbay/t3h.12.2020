9c7ec18793abaecd83c26f9b908c3a2f54a1ed3e
datdofpt <dat.doxuan@ggg.com.vn>
2021-03-28 20:28:11 +0700

    222

---
+++ ngay19/002/index6.php
@@ -0,0 +1,15 @@
+<?php
+
+
+// chuyển từ timestamp về dạng format
+// date(format, timestamp)
+
+
+// chuyển format => timestamp
+$d1 = "12:00:20 12/1/2003";
+$t1 = strtotime($d1);
+echo "<br> d1 : " . $d1;
+echo "<br> t1 : " . $t1;
+echo "<br> chuyển ngược lại " . date("H:i:s d/m/Y", $t1);
+
+// hàm strtotime không hoàn hảo nó chỉ hỗ trợ chuyển 1 số format thời gian nhất định
