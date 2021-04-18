fc1193ebc5ea6dc3460335f95fef50842557a4e5
datdofpt <dat.doxuan@ggg.com.vn>
2021-04-18 20:23:05 +0700

    fff

--- ngay24/002/create.php
+++ ngay24/002/create.php
@@ -1,6 +1,56 @@
 <?php
 require_once "connect.php";
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 
-echo "<pre>";
-print_r($connection);
-echo "</pre>";
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Thêm mới sinh viên</h1>
+                <form action="">
+                    <div class="form-group">
+                        <label>Tên sinh viên</label>
+                        <input type="text" name="" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label>Điểm sinh viên</label>
+                        <input type="text" name="" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label>Nơi ở sinh viên</label>
+                        <input type="text" name="" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label>Sinh nhật sinh viên</label>
+                        <input type="text" name="" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label>Giới tính sinh viên</label>
+                        <input type="radio" value="0" checked> Nữ
+                        <input type="radio" value="1"> Nam
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Thêm mới</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
--- ngay24/002/list.php
+++ ngay24/002/list.php
@@ -1,6 +1,59 @@
 <?php
 require_once "connect.php";
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 
-echo "<pre>";
-print_r($connection);
-echo "</pre>";
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+
+</head>
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Danh sách sinh viên</h1>
+
+                <div style="margin: 20px">
+                    <a href="" class="btn btn-info">Thêm mới sinh viên</a>
+                </div>
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th>Mã SV</th>
+                        <th>Tên SV</th>
+                        <th>Địa điểm</th>
+                        <th>Sinh Nhật</th>
+                        <th>Điểm</th>
+                        <th>Hành động</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>ABC</td>
+                            <td>hà nội</td>
+                            <td>12/12/1995</td>
+                            <td>9.5</td>
+                            <td>
+                                <a href="" class="btn btn-warning">Edit</a>
+                                <a href="" class="btn btn-danger">Delete</a>
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
