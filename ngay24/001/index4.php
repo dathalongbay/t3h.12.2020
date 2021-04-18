07c85eb433592b2bff8cc97739541f83436e0bec
datdofpt <dat.doxuan@ggg.com.vn>
2021-04-18 18:29:33 +0700

    Create index4.php

---
+++ ngay24/001/index4.php
@@ -0,0 +1,6 @@
+<?php
+
+
+// https://dev.mysql.com/doc/refman/8.0/en/charset-database.html
+// CREATE DATABASE db_name CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+// CREATE DATABASE demo1234 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
