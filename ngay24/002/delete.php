09eb89c5098a91e4910898c3cfbe8115ed424b8f
datdofpt <dat.doxuan@ggg.com.vn>
2021-04-23 20:29:47 +0700

    111

--- ngay24/002/delete.php
+++ ngay24/002/delete.php
@@ -4,3 +4,11 @@ require_once "connect.php";
 echo "<pre>";
 print_r($connection);
 echo "</pre>";
+
+$id = isset($_GET["id"]) ? $_GET["id"] : 0;
+$sqlDelete = "DELETE FROM students WHERE student_id=$id";
+echo $sqlDelete;
+$resultInsert = $connection->exec($sqlDelete);
+var_dump($resultInsert);
+header("Location: list.php");
+exit();
