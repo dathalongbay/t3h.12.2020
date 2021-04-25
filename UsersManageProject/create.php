fedb7114481d73097bc5623942e754d9c5d6301f
datdofpt <dat.doxuan@ggg.com.vn>
2021-04-25 18:33:15 +0700

    111

--- UsersManageProject/create.php
+++ UsersManageProject/create.php
@@ -21,52 +21,52 @@
             <h1>Thêm mới người dùng</h1>
             <form name="themsv" method="post" action="" enctype="multipart/form-data">
                 <div class="form-group">
-                    <label>Username người dùng</label>
+                    <label>Username người dùng <span style="color:red">*</span> (từ 6 đên 32 ký tự và không trùng lặp với user khác )</label>
                     <input type="text" name="user_name" class="form-control">
                 </div>
 
                 <div class="form-group">
-                    <label>Lastname người dùng</label>
+                    <label>Lastname người dùng <span style="color:red">*</span> ( họ người dùng từ 1 đến 3 chữ )</label>
                     <input type="text" name="last_name" class="form-control">
                 </div>
 
                 <div class="form-group">
-                    <label>Firstname người dùng</label>
+                    <label>Firstname người dùng <span style="color:red">*</span> (tên người dùng từ 1 đến 2 chữ )</label>
                     <input type="text" name="first_name" class="form-control">
                 </div>
 
                 <div class="form-group">
-                    <label>SDT người dùng</label>
+                    <label>SDT người dùng <span style="color:red">*</span> ( 10 số và không trùng lặp với user khác )</label>
                     <input type="text" name="user_phone" class="form-control">
                 </div>
 
                 <div class="form-group">
-                    <label>Mật khẩu người dùng</label>
+                    <label>Mật khẩu người dùng <span style="color:red">*</span> ( từ 8 đến 32 ký tự có chứa ít nhất 1 ký tự viết hoa,chứa ít nhất 1 số )</label>
                     <input type="password" name="user_password" class="form-control">
                 </div>
 
                 <div class="form-group">
-                    <label>Email người dùng</label>
+                    <label>Email người dùng <span style="color:red">*</span> (tối đa 64 ký tự theo định dạng email : abc@domain.com và không trùng lặp với user khác)</label>
                     <input type="email" name="user_email" class="form-control">
                 </div>
 
                 <div class="form-group">
-                    <label>Ảnh đại diện</label>
+                    <label>Ảnh đại diện <span style="color:red">*</span></label>
                     <input type="file" name="user_avatar">
                 </div>
 
                 <div class="form-group">
-                    <label>Nơi ở người dùng</label>
+                    <label>Nơi ở người dùng <span style="color:red">*</span> ( tối đa 200 chữ )</label>
                     <input type="text" name="user_address" class="form-control">
                 </div>
 
                 <div class="form-group">
-                    <label>Sinh nhật người dùng</label>
+                    <label>Sinh nhật người dùng <span style="color:red">*</span></label>
                     <input type="date" name="user_birthday" class="form-control">
                 </div>
 
                 <div class="form-group">
-                    <label>Giới tính người dùng</label>
+                    <label>Giới tính người dùng <span style="color:red">*</span></label>
                     <input type="radio" name="user_gender" value="0" checked> Nữ
                     <input type="radio" name="user_gender" value="1"> Nam
                 </div>
