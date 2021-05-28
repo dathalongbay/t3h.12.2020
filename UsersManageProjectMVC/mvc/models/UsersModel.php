8e6aaf366c4d6f985457dfbdb51c05cc0c2ef8a9
datdofpt <dat.doxuan@ggg.com.vn>
2021-05-28 21:16:08 +0700

    hoàn thiện model

--- UsersManageProjectMVC/mvc/models/UsersModel.php
+++ UsersManageProjectMVC/mvc/models/UsersModel.php
@@ -2,6 +2,7 @@
 
 class UsersModel extends Database {
 
+    // model chỉ tương tác với CSDL hay tính toán hay thực thi logic
     // tên bảng
     public $table = "users";
 
@@ -10,29 +11,85 @@ class UsersModel extends Database {
 
         $sqlSelect = "SELECT * FROM $this->table";
 
+        $stmt = $this->connection->prepare($sqlSelect);
+
+        $stmt->execute();
+
+        $result = $stmt->setFetchMode(PDO::FETCH_OBJ);
+
+        $users = $stmt->fetchAll();
+
+        return $users;
     }
 
     // lấy ra 1 bản ghi duy nhất
     public function getSingle($id) {
 
-        $sqlSelect = "SELECT * FROM $this->table WHERE id = ?";
+        $sqlSelect = "SELECT * FROM $this->table WHERE id = ? LIMIT 1";
 
+        $stmt = $this->connection->prepare($sqlSelect);
+
+        $stmt->execute([$id]);
+
+        $result = $stmt->setFetchMode(PDO::FETCH_OBJ);
+
+        $user = $stmt->fetchObject();
+
+        return $user;
     }
 
     // lưu mới 1 bản ghi
-    public function store() {
+    // hàm là tập hợp các câu lệnh để thực hiện 1 chức năng
+    // hàm có input và output
+    // input là tham số
+    // output là return cuối hàm
+    public function store(array $dataBind) {
 
-        $sqlCreate = "INSERT INTO $this->table ( `user_name`, `first_name`, `last_name`, `user_email`, `user_gender`, `user_phone`, `user_address`, `user_password`, `user_avatar`, `user_birthday`, `user_desc`, `created`, `updated`) VALUES ( ?, ?, ?, ?, ?, ? , ? , ? , ? , ? , ? , ? , ?)";
+        $sqlInsert = "INSERT INTO $this->table ( `user_name`, `first_name`, `last_name`, `user_email`, `user_gender`, `user_phone`, `user_address`, `user_password`, `user_avatar`, `user_birthday`, `user_desc`, `created`, `updated`) VALUES ( ?, ?, ?, ?, ?, ? , ? , ? , ? , ? , ? , ? , ?)";
 
+        $stmtInsert = $this->connection->prepare($sqlInsert);
+
+        $resultInsert = $stmtInsert->execute($dataBind);
+
+        // kết quả thực thi câu sql insert
+        return $resultInsert;
     }
 
 
     // update 1 bản ghi
-    public function update() {
+    public function update(array $dataBindSql) {
 
-        $sqlUpdate = "UPDATE $this->table `user_name` = ?, `first_name` = ? ";
+        // sql update va sql delete
+        $sqlUpdate = "UPDATE $this->table SET ";
+
+        $sqlUpdate .= "`user_name` = ? ";
+
+        $sqlUpdate .= ",`first_name` = ?";
+
+        $sqlUpdate .= ",`last_name` = ?";
+
+        $sqlUpdate .= ", `user_email` = ?";
+
+        $sqlUpdate .= ", `user_gender` = ?";
+
+        $sqlUpdate .= ", `user_phone` = ? ";
+
+        $sqlUpdate .= ", `user_address` = ?";
+
+        $sqlUpdate .= ",`user_birthday` =?";
+
+        $sqlUpdate .= ",`user_desc` = ?";
+
+        $sqlUpdate .= ",`updated` = ?";
+
         $sqlUpdate .= " WHERE `user_id` = ?";
 
+        $stmtInsert = $this->connection->prepare($sqlUpdate);
+
+        $resultUpdate = $stmtInsert->execute($dataBindSql);
+
+        // output
+        return $resultUpdate;
     }
 
 
@@ -41,5 +98,11 @@ class UsersModel extends Database {
 
         $sqlDelete = "DELETE FROM $this->table WHERE `user_id` = ?";
 
+        $stmtDelete = $this->connection->prepare($sqlDelete);
+
+        $resultDelete = $stmtDelete->execute([$id]);
+
+        // output
+        return $resultDelete;
     }
 }
