bade465280aa62ff953404f3e532f08c8d0a6b29
datdofpt <dat.doxuan@ggg.com.vn>
2021-05-28 20:17:57 +0700

    111

---
+++ UsersManageProjectMVC/index.php
@@ -0,0 +1,5 @@
+<?php
+// no mvc giống là ngôi nhà có rất nhiều cửa ra vào
+// mvc thì có 1 cửa chính để ra vào duy nhất
+// index.php là file đầu vào của ứng dụng
+// mọi tác vụ thêm sửa xóa đăng nhập đăng ký đều chạy file index.php ngoài cùng
